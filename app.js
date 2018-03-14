aff93cd1527e3251f99a340567f1c0a7f0beb396
Anthony Rivas <kamikazebot@gmail.com>
2018-03-13 23:47:59 -0700

    Adding cors headers

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
 var express = require('express'),
-    app = express(),
-    request = require('request');
-    fs = require('fs'),
-    bp = require('body-parser'),
-    form = fs.readFileSync(__dirname + '/form.html').toString();
+  app = express(),
+  request = require('request'),
+  fs = require('fs'),
+  bp = require('body-parser'),
+  form = fs.readFileSync(__dirname + '/form.html').toString();
 
 app.use(bp.json());
 
-app.get('/', function(req, res) {
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+});
+
+app.get('/', function (req, res) {
   res.set('Content-Type', 'text/html');
   res.send(form);
 });
 
 app.get('/:url', function (req, res) {
-  if(req.params.url && req.params.url.indexOf('http') >= 0) {
+  if (req.params.url && req.params.url.indexOf('http') >= 0) {
     request(req.params.url).pipe(res);
   } else {
     res.send('Invalid URL');
@@ -21,7 +37,7 @@ app.get('/:url', function (req, res) {
 });
 
 app.post('/', function (req, res) {
-  if(req.body && req.body.url) {
+  if (req.body && req.body.url) {
     request(req.body.url).pipe(res);
   } else {
     res.send('Invalid Request, brah');
@@ -32,6 +48,6 @@ app.all('*', function (req, res) {
   res.send('Invalid Request, brah');
 });
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening!')
 });
