f15945221b94c1e99c0d865c5fab418411d90214
Busyrich <rich@busyrich.com>
2018-03-13 21:14:41 -0700

    Added the proper start script for this to work on Heroku

--- app.js
+++ app.js
@@ -32,6 +32,6 @@ app.all('*', function (req, res) {
   res.send('Invalid Request, brah');
 });
 
-app.listen(3000, function() {
-  console.log('Example app listening on port 3000!')
+app.listen(process.env.PORT || 3000, function() {
+  console.log('Example app listening!')
 });
